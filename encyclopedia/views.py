173f02dc9a1cec3a21fdea97932073cacae65992
pvpstory <koliyayyt@gmail.com>
2024-01-09 15:48:18 +0100

    find an entry by an url

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from . import util
@@ -6,5 +7,11 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+def wiki(request, title):
+    if util.get_entry(title) is None:
+        return HttpResponse("error")
+    return render(request, "encyclopedia/wiki.html", {
+        "content": util.get_entry(title), "title": title
     })
 
