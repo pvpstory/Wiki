3c74007b1a4fb817a61701af66e2164e745bda6c
pvpstory <koliyayyt@gmail.com>
2024-03-13 16:21:54 +0100

    search part 1

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,17 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django import forms
 from . import util
 
-
+class NewTaskForm(forms.Form):
+    title = forms.CharField(label='title')
+#<input class="search" type="text" name="q" placeholder="Search Encyclopedia">
 def index(request):
+
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            return render(request, "encyclopedia/wiki.html", {
+                "content": util.get_entry(title), "title": title
+            })
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(), "form": NewTaskForm()
+
     })
+
 def wiki(request, title):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            return render(request, "encyclopedia/wiki.html", {
+                "content": util.get_entry(title), "title": title
+            })
+
     if util.get_entry(title) is None:
         return HttpResponse("error")
     return render(request, "encyclopedia/wiki.html", {
-        "content": util.get_entry(title), "title": title
+        "content": util.get_entry(title), "title": title, "form": NewTaskForm()
     })
+
 
